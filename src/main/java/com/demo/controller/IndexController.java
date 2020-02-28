c94172bf84cc184a9df15ffa568f2584fb1a52cc
zouzou <984694620@qq.com>
2020-02-28 11:37:05 +0800

    分页，个人资料，拦截器，问题详情初步

--- src/main/java/com/demo/controller/IndexController.java
+++ src/main/java/com/demo/controller/IndexController.java
@@ -1,50 +1,25 @@
 package com.demo.controller;
 
-import com.demo.dto.QuestionDTO;
-import com.demo.mapper.QuestionMapper;
-import com.demo.mapper.UserMapper;
-import com.demo.model.Question;
-import com.demo.model.User;
+import com.demo.dto.PaginationDTO;
 import com.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
     @Autowired
-    private  UserMapper userMapper;
-
-    @Autowired
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String hello(HttpServletRequest request,
-                        Model model){
-        Cookie[] cookies = request.getCookies();
-        if (cookies!=null && cookies.length != 0){
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")){
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null){
-                        request.getSession().setAttribute("user",user);
-                    }
-                    break;
-                }
-            }
-
-        }
-
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
-
+    public String hello(Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "5") Integer size){
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
 
     }
--- src/main/java/com/demo/service/QuestionService.java
+++ src/main/java/com/demo/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.demo.service;
 
+import com.demo.dto.PaginationDTO;
 import com.demo.dto.QuestionDTO;
 import com.demo.mapper.QuestionMapper;
 import com.demo.mapper.UserMapper;
@@ -20,9 +21,30 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        Integer totalPage;
+        if (totalCount%size==0){
+            totalPage=totalCount/size;
+        }else {
+            totalPage=totalCount/size+1;
+        }
+
+        if (page<1){
+            page=1;
+        }
+        if (page>totalPage){
+            page=totalPage;
+        }
+        paginationDTO.setPagination(totalPage,page);
+        //size*(page-1)
+        Integer offset = size* (page-1);
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -30,6 +52,50 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
+    }
+
+    public PaginationDTO list(Integer userId, Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalPage;
+        Integer totalCount = questionMapper.countByUserId(userId);
+        if (totalCount%size==0){
+            totalPage=totalCount/size;
+        }else {
+            totalPage=totalCount/size+1;
+        }
+
+        if (page<1){
+            page=1;
+        }
+        if (page>totalPage){
+            page=totalPage;
+        }
+        paginationDTO.setPagination(totalPage,page);
+        //size*(page-1)
+        Integer offset = size* (page-1);
+        List<Question> questions = questionMapper.listByUserId(userId,offset,size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
+    }
+
+    public QuestionDTO getById(Integer id) {
+        Question question = questionMapper.getById(id);
+        QuestionDTO questionDTO = new QuestionDTO();
+        BeanUtils.copyProperties(question,questionDTO);
+        return questionDTO;
     }
 }
